99c84acec32cb79f072601172dab847d13981a8d
juamatx <60664640+juamatx@users.noreply.github.com>
2023-02-27 08:01:33 -0500

    create utils.py

---
+++ utils.py
@@ -0,0 +1,33 @@
+
+import boto3
+import json
+
+#  Returns the value a given secret
+def get_secret(secret_name):
+
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    # Converts string to json/dict and returns it
+    return json.loads(secret)[secret_name]
+
+
+print('hello world')
